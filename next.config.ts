ecbdafa8b63e9082aa37ec87e2322de6ebc96114
Larry Margerum <larrymargerum1950@gmail.com>
2026-08-19 20:04:20 +0200

    Cloudflare Deploy Environment Setup

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    unoptimized: true
   },
 };
 
